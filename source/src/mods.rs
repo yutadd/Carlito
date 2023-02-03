59f2d3665e7e3e06bb53e4d2e1d6e00ab275ef56
yutadd <spyk4m49@gmail.com>
2023-02-03 18:33:51 +0900

    update certification

--- source/src/mods.rs
+++ source/src/mods.rs
@@ -1,3 +1,5 @@
 pub mod network;
 pub mod config_wrapper;
 pub mod certification;
+pub mod util;
+pub mod file_system;
--- source/src/mods/certification/key_agent.rs
+++ source/src/mods/certification/key_agent.rs
@@ -1,33 +1,49 @@
 use secp256k1::{Secp256k1, Message,SecretKey, PublicKey};
-use std::{str::FromStr, ptr::null_mut, fmt::format};
+use std::{str::FromStr, ptr::null_mut, fmt::format, path::Path};
 use rand::rngs::OsRng;
 use once_cell::sync::Lazy;
+use std::env;
+use std::fs::File;
+use crate::mods::util::system;
+use crate::mods::file_system::file_controll::{self, get_file};
+use std::io::{prelude::*, ErrorKind};
+use std::io::BufReader;
 pub static SECRET:Vec<Lazy<SecretKey>>=Vec::new();
+
 /*  
     implements later from https://github.com/rust-bitcoin/rust-secp256k1/blob/master/examples/sign_verify_recovery.rs
     and https://docs.rs/secp256k1/latest/secp256k1/ 
 */
 //create file or read file
 pub fn init(){
-    add_new_key();
-   //ファイルの存在を確認し、file_not_existsかread_key_from_file()か振り分けをする。
+    let f;
+    f=get_file("../secret/secret.txt".to_string());
+    read_key_from_file(f);
 }
-fn file_not_exists(){
-    //ユーザーへ鍵を作成するか確認し、処理を振り分ける。
-}
-fn add_new_key(){
+fn create_new_key(){
+    let f=get_file("../secret/secret.txt".to_string());
     let secp = Secp256k1::new();
     let (secret_key, public_key) = secp.generate_keypair(&mut OsRng);
     println!("{:?}",secret_key.display_secret());
     println!("{:?}",public_key.serialize());
-    append_key_to_file(secret_key)
+    append_key_to_file(secret_key,f);
 }
-fn read_key_from_file()->Vec<SecretKey>{
+fn read_key_from_file(file:File)->Vec<&'static SecretKey>{
+    let mut svec:Vec<&'static SecretKey>=  Vec::new();
+    let reader = BufReader::new(file);
+    for line in reader.lines() {
+        let line = line.unwrap();
+        let key:&'static SecretKey;
+        match SecretKey::from_str(&line){
+            Ok(sec)=>key=&sec,
+            Err(E)=>system::exit_with_error(E.to_string())
+        };
+        svec.push(&key);
+    }
+    svec
     
-    let key = SecretKey::from_str("0000000000000000000000000000000000000000000000000000000000000001").unwrap();
-    Vec::new()
 }
-fn append_key_to_file(key:SecretKey){
+fn append_key_to_file(key:SecretKey,file:File){
     let secret_str=format!("{}\n",key.display_secret());
     
 }
