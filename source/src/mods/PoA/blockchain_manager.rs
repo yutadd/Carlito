e9d470be07266a912fbac04db0a8a8536b6bc6b1
yutadd <spyk4m49@gmail.com>
2023-02-19 23:19:21 +0900

    update block generation system

--- source/src/mods/PoA/blockchain_manager.rs
+++ source/src/mods/PoA/blockchain_manager.rs
@@ -18,51 +18,70 @@ use secp256k1::Message;
 pub static mut TRANSACTION_POOL: Lazy<Vec<JsonValue>> = Lazy::new(|| Vec::new());
 pub static mut PREVIOUS_GENERATOR: isize = -1; //ブロック読み込みや受け取り時に更新するべし
 pub fn block_generate() {
-    loop {
-        if connection::is_all_connected() {
-            unsafe {
+    unsafe {
+        loop {
+            if connection::is_all_connected() || BLOCKCHAIN.len() > 0 {
+                //ブロックの読み込みと準備が完了しているか
                 if !PREVIOUS_GENERATOR.eq(&-1) {
                     let mut next_index = -1;
+
+                    //信用リスト内部における次の生成者の添字を算出する
                     if PREVIOUS_GENERATOR < (TRUSTED_KEY.len() - 1).try_into().unwrap() {
                         next_index = PREVIOUS_GENERATOR + 1;
                     } else {
                         next_index = 0;
                     }
+
+                    //算出された次の生成車は自分か
                     if TRUSTED_KEY
                         .get(&next_index)
                         .unwrap()
                         .eq(&key_agent::SECRET[0].public_key(&SECP).to_string())
                     {
-                        let mut block = object![
-                            previous_hash:Message::from_hashed_data::<sha256::Hash>(BLOCKCHAIN[BLOCKCHAIN.len()-1].dump().as_bytes()).to_string(),
-                            author:SECRET[0].public_key(&SECP).to_string(),
-                            date:Utc::now().timestamp_millis(),
-                            height:BLOCKCHAIN[BLOCKCHAIN.len()-1]["height"].as_usize().unwrap()+1,
-                            transactions:array![],
-                        ];
-                        block
-                            .insert("sign", create_sign(block.dump(), SECRET[0]).to_string())
-                            .unwrap();
-                        assert!(check(
-                            block.clone(),
-                            Message::from_hashed_data::<sha256::Hash>(
-                                BLOCKCHAIN[BLOCKCHAIN.len() - 1].dump().as_bytes(),
-                            )
-                            .to_string(),
-                        ));
-                        println("[blockchain_manager]block generated successfully");
-                        BLOCKCHAIN.push(block.clone());
-                        for c in CONNECTION_LIST.iter() {
-                            c.write(format!(
-                                "{{\"type\":\"block\",\"args\":{{\"block\":{}}}}}\r\n",
-                                block.dump()
+                        //一つ前のブロックが生成された時間から+10000ミリ秒以上過ぎているか
+                        if NaiveDateTime::from_timestamp_millis(
+                            (BLOCKCHAIN[BLOCKCHAIN.len() - 1]["date"].as_isize().unwrap() + 10000)
+                                .try_into()
+                                .unwrap(),
+                        )
+                        .unwrap()
+                        .cmp(
+                            &NaiveDateTime::from_timestamp_millis(Utc::now().timestamp_millis())
+                                .unwrap(),
+                        )
+                        .is_lt()
+                        {
+                            //ブロック生成
+                            let mut block = object![
+                                previous_hash:Message::from_hashed_data::<sha256::Hash>(BLOCKCHAIN[BLOCKCHAIN.len()-1].dump().as_bytes()).to_string(),
+                                author:SECRET[0].public_key(&SECP).to_string(),
+                                date:Utc::now().timestamp_millis(),
+                                height:BLOCKCHAIN[BLOCKCHAIN.len()-1]["height"].as_usize().unwrap()+1,
+                                transactions:array![],
+                            ];
+                            block
+                                .insert("sign", create_sign(block.dump(), SECRET[0]).to_string())
+                                .unwrap();
+                            assert!(check(
+                                block.clone(),
+                                Message::from_hashed_data::<sha256::Hash>(
+                                    BLOCKCHAIN[BLOCKCHAIN.len() - 1].dump().as_bytes(),
+                                )
+                                .to_string(),
                             ));
+                            println("[blockchain_manager]block generated successfully");
+                            BLOCKCHAIN.push(block.clone());
+                            for c in CONNECTION_LIST.iter() {
+                                c.write(format!(
+                                    "{{\"type\":\"block\",\"args\":{{\"block\":{}}}}}\r\n",
+                                    block.dump()
+                                ));
+                            }
                         }
                     }
-                    thread::sleep(Duration::from_secs(1));
+                    thread::sleep(Duration::from_secs(4));
                 } else {
-                    println(format!("[blockchain_manager]preloaded chain is not ready"));
-                    thread::sleep(Duration::from_secs(8));
+                    println(format!("[blockchain_manager]waiting for load local block"));
                     let mut latest_nodes = 0;
                     let mut trusted_nodes = 0;
                     for c in CONNECTION_LIST.iter() {
@@ -76,7 +95,6 @@ pub fn block_generate() {
                         }
                     }
                     if latest_nodes == trusted_nodes {
-                        println("[blockchain_manager]all node latest");
                         let author = BLOCKCHAIN[BLOCKCHAIN.len() - 1]["author"].to_string();
                         for i in 0..TRUSTED_KEY.len() {
                             if TRUSTED_KEY.get(&(i as isize)).unwrap().eq(&author) {
@@ -89,13 +107,14 @@ pub fn block_generate() {
                             latest_nodes, trusted_nodes
                         ));
                     }
+                    thread::sleep(Duration::from_secs(4));
                 }
+            } else {
+                println(format!(
+                    "[blockchain_manager]waiting connection and prepare blockchain"
+                ));
+                thread::sleep(Duration::from_secs(4));
             }
-        } else {
-            println(format!(
-                "[blockchain_manager]waiting connection for start generate block."
-            ));
-            thread::sleep(Duration::from_secs(8));
         }
     }
 }
